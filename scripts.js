6b6e813b177adafeb127202decda8d5ab3336113
Eduardo Cezar <ecezar07@gmail.com>
2022-04-25 09:13:35 -0300

    add files

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-const tela = window;
-const container = document.querySelectorAll('[data-container]');
-console.log(window);
-console.log(container);
+// const tela = window;
+// const container = document.querySelectorAll('[data-container]');
+// console.log(window);
+// console.log(container);
 
-function removeContainer() {
-  if (tela.innerWidth < 1250) {
-    container.forEach((item) => {
-      item.classList.remove('container');
-    });
-  } else {
-    container.forEach((item) => {
-      item.classList.add('container');
-    });
-  }
-}
+// function removeContainer() {
+//   if (tela.innerWidth < 1250) {
+//     container.forEach((item) => {
+//       item.classList.remove('container');
+//     });
+//   } else {
+//     container.forEach((item) => {
+//       item.classList.add('container');
+//     });
+//   }
+// }
 
-removeContainer();
+// removeContainer();
